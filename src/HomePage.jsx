42c003c687240833b2985f6c2e0ba03b2e1a5438
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-21 21:30:32 +0530

    Refactor HomePage layout by adjusting spacing and alignment for improved aesthetics

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -49,23 +49,23 @@ function HomePage() {
         <h1 className="text-[#5db7fc] md:text-5xl  text-3xl">
           Chain <span className="text-[#eb0bf9]">Submmit</span>
         </h1>
-        <p className="text-white md:pt-6 md:text-lg mt-4 ">
+        <p className="text-white md:pt-6 md:text-lg mt-5 ">
           Where Artificial Intelligence Meets Blockchain Innovation
         </p>
-        <div className="flex justify-center md:gap-15 text-[#5de2e0] text-2xl font-bold md:mt-10">
+        <div className="flex justify-center md:gap-15 gap-12 text-[#5de2e0] text-2xl font-bold md:mt-10 mt-20">
           <p>500</p>
           <p>20</p>
           <p>3</p>
           <p>56</p>
         </div>
-        <div className="flex justify-center md:gap-7 text-amber-50 font-light md:mt-5">
+        <div className="flex justify-center md:gap-7 gap-4 text-amber-50 font-light md:mt-5">
           <p>Attendess</p>
           <p>Speakers</p>
           <p>Days</p>
           <p>Sponsors</p>
         </div>
-        <div className="flex justify-center md:gap-7 text-[#5de2e0]  md:mt-12 text-2xl font-bold ">
-          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg md:pt-2  hover:shadow-lg shadow-blue-400">
+        <div className="flex justify-center md:gap-7 gap-4 text-[#5de2e0]  md:mt-12 mt-15 ml-3 mr-3 text-2xl font-bold ">
+          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg  pt-2 hover:shadow-lg shadow-blue-400">
             372
             <br />
             <span className="text-gray-300 text-sm font-light md:pt-5">
@@ -73,21 +73,21 @@ function HomePage() {
             </span>
           </p>
 
-          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg md:pt-2  hover:shadow-lg shadow-blue-400">
+          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg pt-2  hover:shadow-lg shadow-blue-400">
             09
             <br />
             <span className="text-gray-300 text-sm font-light md:pt-5">
               Hours
             </span>
           </p>
-          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg md:pt-2  hover:shadow-lg shadow-blue-400">
+          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg pt-2  hover:shadow-lg shadow-blue-400">
             32
             <br />
             <span className="text-gray-300 text-sm font-light md:pt-5">
               Minutes
             </span>
           </p>
-          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg md:pt-2  hover:shadow-lg shadow-blue-400">
+          <p className="bg-[#1f1e23] w-20 h-20 rounded-lg pt-2  hover:shadow-lg shadow-blue-400">
             00
             <br />
             <span className="text-gray-300 text-sm font-light md:pt-5">
@@ -96,12 +96,12 @@ function HomePage() {
           </p>
         </div>
       </div>
-      <div className=" flex ml-150 mt- gap-30">
+      <div className=" flex md:ml-150 justify-center mt-20 gap-20 md:gap-30">
         <button className="px-3 py-2  bg-linear-to-r from-[#50d8fe] to-[#6764f3]  rounded-3xl text-sm">
           Learn More{" "}
         </button>
 
-        <button className="md:border-2 md:border-solid border-[#3a9994] rounded-3xl text-white  md:text-sm md:px-4 md:py-2 hover:shadow shadow-blue-400">
+        <button className="border-2 border-solid border-[#3a9994] rounded-3xl text-white  text-sm md:px-4 px-2 py-2  hover:shadow shadow-blue-400">
           View Schedule
         </button>
       </div>
