df5bff91c93f5f9ea1d89444b917ea927b1bfc4a
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-21 13:03:23 +0530

    Add navigation to About section in MenuBar component

--- src/MenuBar.jsx
+++ src/MenuBar.jsx
@@ -1,16 +1,24 @@
 import { FaXmark } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
+
 
 function MenuBar() {
+
+  const navigate = useNavigate();
+
   const closebar = () => {
     window.history.back();
   };
+  const about =() =>{
+     navigate("/about");
+  }
   return (
     <div className="  text-white md:hidden ">
       <div className=" relative ml-76 mt-5">
         <FaXmark size={30} onClick={closebar} />
       </div>
       <div className="absolute top-20 left-20  text-3xl">
-        <p className="mb-2 "> About</p>
+        <p className="mb-2 " onClick={about}> About</p>
         <p className="mb-2 "> Speakers</p>
         <p className="mb-2 "> Schedule</p>
         <p className="mb-2 "> Sponsors</p>
