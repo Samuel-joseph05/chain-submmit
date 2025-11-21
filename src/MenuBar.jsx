dc990f777cb934cfbb19b64fe75069cbe33459a0
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-21 13:05:13 +0530

    Add click functionality to Speakers link in MenuBar component

--- src/MenuBar.jsx
+++ src/MenuBar.jsx
@@ -12,6 +12,10 @@ function MenuBar() {
   const about =() =>{
      navigate("/about");
   }
+    const speakers = () => {
+    navigate("/speakers");
+  };
+  
   return (
     <div className="  text-white md:hidden ">
       <div className=" relative ml-76 mt-5">
@@ -19,7 +23,7 @@ function MenuBar() {
       </div>
       <div className="absolute top-20 left-20  text-3xl">
         <p className="mb-2 " onClick={about}> About</p>
-        <p className="mb-2 "> Speakers</p>
+        <p className="mb-2 "    onClick={speakers}> Speakers</p>
         <p className="mb-2 "> Schedule</p>
         <p className="mb-2 "> Sponsors</p>
         <p className=""> Register</p>
