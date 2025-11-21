f7e1664f2ad2116652209c325b9ba3a0477b6bea
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-21 13:04:16 +0530

    Add SpeakersPage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./HomePage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MenuBar from "./MenuBar";
 import AboutPage from "./AboutPage";
+import SpeakersPage from "./SpeakersPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/menubar" element={<MenuBar />} />
         <Route path="/about" element={<AboutPage />} />
+           <Route path="/speakers" element={<SpeakersPage />} />
       </Routes>
     </Router>
   );
