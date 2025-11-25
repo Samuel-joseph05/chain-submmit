8220373658302a70194c9a037e0094aad758f46b
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-26 00:10:40 +0530

    Add routes for Schedule and Sponsors pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MenuBar from "./MenuBar";
 import AboutPage from "./AboutPage";
 import SpeakersPage from "./SpeakersPage";
+import Schedule from "./Schedule";
+import Sponsors from "./Sponsors";
+
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/menubar" element={<MenuBar />} />
         <Route path="/about" element={<AboutPage />} />
-           <Route path="/speakers" element={<SpeakersPage />} />
+        <Route path="/speakers" element={<SpeakersPage />} />
+        <Route path="/schedule" element={<Schedule />} />
+         <Route path="/sponsors" element={<Sponsors/>} />
+        
       </Routes>
     </Router>
   );
