dd4fce7e68bd58a8b7ccee23b51d8312b91fd9a0
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-26 00:22:21 +0530

    feat: add Sponsor page

---
+++ src/Sponsors.jsx
@@ -0,0 +1,13 @@
+
+
+function Sponsors() {
+  return (
+    <div>
+      <div className="text-center pt-5"> 
+        <h1 className="text-[#62b4f6] text-2xl">Our <span className="text-[#7757d1]">Sponsors</span></h1>
+      </div>
+    </div>
+  )
+}
+
+export default Sponsors
