c8e4e712a18e4d87bfde5dbba9c7e13af6c32548
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-26 00:19:37 +0530

    feat: add navigation handler

--- src/MenuBar.jsx
+++ src/MenuBar.jsx
@@ -1,31 +1,47 @@
 import { FaXmark } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 
-
 function MenuBar() {
-
   const navigate = useNavigate();
 
   const closebar = () => {
     window.history.back();
   };
-  const about =() =>{
-     navigate("/about");
-  }
-    const speakers = () => {
+  const about = () => {
+    navigate("/about");
+  };
+  const speakers = () => {
     navigate("/speakers");
   };
-  
+  const schedule = () => {
+    navigate("/schedule");
+  };
+   const sponsors = () => {
+    navigate("/sponsors");
+  };
+
   return (
     <div className="  text-white md:hidden ">
-      <div className=" relative ml-76 mt-5">
+      <div className=" relative ml-85 sm:ml-100 mt-5">
         <FaXmark size={30} onClick={closebar} />
       </div>
-      <div className="absolute top-20 left-20  text-3xl">
-        <p className="mb-2 " onClick={about}> About</p>
-        <p className="mb-2 "    onClick={speakers}> Speakers</p>
-        <p className="mb-2 "> Schedule</p>
-        <p className="mb-2 "> Sponsors</p>
+      <div className="absolute top-20 right-40  text-3xl">
+        <p className="mb-2 " onClick={about}>
+          {" "}
+          About
+        </p>
+        <p className="mb-2 " onClick={speakers}>
+          {" "}
+          Speakers
+        </p>
+        <p className="mb-2 " onClick={schedule}>
+          {" "}
+          Schedule
+        </p>
+        <p className="mb-2 " onClick={sponsors}>
+          {" "}
+          Sponsors
+        </p>
         <p className=""> Register</p>
       </div>
     </div>
