abd487def68183c3751f5ecb6f5f1986d98dfd4c
Samuel-joseph05 <samuela05022004@gmail.com>
2025-11-28 16:21:00 +0530

    style(ui): adjust padding, margin, and component styles

--- src/Sponsors.jsx
+++ src/Sponsors.jsx
@@ -40,7 +40,7 @@ function Sponsors() {
               </p>
             </div>
           </div>
-          <div className="text-center md:p-5 p-5">
+          <div className="text-center md:p-5 p-7 mt-10">
             <h1 className="text-yellow-200 md:text-2xl font-bold">
               Gold Sponsors
             </h1>
@@ -49,11 +49,11 @@ function Sponsors() {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-3">
             <div className=" w-auto  h-60 md:h-60 md:w-70  md:mx-50 mx-12 bg-[#253036] border-2 border-solid border-[#393e48] rounded-2xl">
               <div className="w-auto h-30 mx-10 mt-7 bg-[#e38e02] text-black text-2xl shadow-lg shadow-amber-600 place-content-center text-center rounded-2xl font-bold">
-                TC
+                DM
               </div>
               <div>
                 <h3 className="text-center text-[#dde3e8] font-medium mt-4">
-                  Techcorp Global
+                  DataMind
                 </h3>
                 <p className=" text-center text-gray-400 font-light mt-3">
                   {" "}
@@ -63,11 +63,11 @@ function Sponsors() {
             </div>
             <div className=" w-auto  h-60 md:h-60 md:w-70 md:mx-50 mx-12  bg-[#253036] border-2 border-solid border-[#393e48] rounded-2xl">
               <div className="w-auto h-30 mx-10 mt-7 bg-[#e38e02] text-black text-2xl shadow-lg shadow-amber-600 place-content-center text-center rounded-2xl font-bold">
-                TC
+                CL
               </div>
               <div>
                 <h3 className="text-center text-[#dde3e8] font-medium mt-4">
-                  Techcorp Global
+                  CryptoLabs
                 </h3>
                 <p className=" text-center text-gray-400 font-light mt-3 ">
                   {" "}
